24616c1927eddcb9abdb4f506dd13ebe643f7a42
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2023-12-17 12:05:47 +0100

    Testing black formatter

--- console.py
+++ console.py
@@ -55,12 +55,12 @@ class HBNBCommand(cmd.Cmd):
             _cls = pline[: pline.find(".")]
 
             # isolate and validate <command>
-            _cmd = pline[pline.find(".") + 1: pline.find("(")]
+            _cmd = pline[pline.find(".") + 1 : pline.find("(")]
             if _cmd not in HBNBCommand.dot_cmds:
                 raise Exception
 
             # if parantheses contain arguments, parse them
-            pline = pline[pline.find("(") + 1: pline.find(")")]
+            pline = pline[pline.find("(") + 1 : pline.find(")")]
             if pline:
                 # partition args: (<id>, [<delim>], [<*args>])
                 pline = pline.partition(", ")  # pline convert to tuple
@@ -129,7 +129,7 @@ class HBNBCommand(cmd.Cmd):
                 if second_quote > 0:
                     value = args[1:second_quote]
                     value = value.replace('\\"', '"').replace("_", " ")
-                    args = args[second_quote + 1:].strip()
+                    args = args[second_quote + 1 :].strip()
             elif args:
                 value = args.split()[0]
                 if "." in value:
@@ -318,7 +318,7 @@ class HBNBCommand(cmd.Cmd):
             if args and args[0] == '"':  # check for quoted arg
                 second_quote = args.find('"', 1)
                 att_name = args[1:second_quote]
-                args = args[second_quote + 1:]
+                args = args[second_quote + 1 :]
 
             args = args.partition(" ")
 
@@ -327,7 +327,7 @@ class HBNBCommand(cmd.Cmd):
                 att_name = args[0]
             # check for quoted val arg
             if args[2] and args[2][0] == '"':
-                att_val = args[2][1: args[2].find('"', 1)]
+                att_val = args[2][1 : args[2].find('"', 1)]
 
             # if att_val was not quoted arg
             if not att_val and args[2]:
