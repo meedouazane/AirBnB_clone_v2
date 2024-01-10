45d62e000f7a7f2853f32aff34500d03ff3e1978
meedouazane <mohamedouazane@yahoo.com>
2024-01-10 14:30:27 -0800

    2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,22 +8,21 @@ env.hosts = ['54.167.181.186', '35.153.194.155']
 
 def do_deploy(archive_path):
     """ Deploy archive! """
-    if exists(archive_path) is True:
-        try:
-            put(archive_path, '/tmp/')
-            file_name_ext = archive_path.split('/')[1]
-            file_name = file_name_ext.split('.')[0]
-            run(f"mkdir -p /data/web_static/releases/{file_name}/")
-            run(f" tar -xzf /tmp/{file_name_ext} -C "
-                f"/data/web_static/releases/{file_name}/")
-            run(f"rm -rf /tmp/{file_name_ext}")
-            run(f"mv /data/web_static/releases/{file_name}/web_static/* "
-                f"/data/web_static/releases/{file_name}/")
-            run("rm -rf /data/web_static/current")
-            run(f"ln -s /data/web_static/releases/{file_name}/"
-                f" /data/web_static/current")
-            return True
-        except Exception:
-            return False
-    else:
+    if exists(archive_path) is False:
+        return False
+    try:
+        put(archive_path, '/tmp/')
+        file_name_ext = archive_path.split('/')[1]
+        file_name = file_name_ext.split('.')[0]
+        run(f"mkdir -p /data/web_static/releases/{file_name}/")
+        run(f" tar -xzf /tmp/{file_name_ext} -C "
+            f"/data/web_static/releases/{file_name}/")
+        run(f"rm -rf /tmp/{file_name_ext}")
+        run(f"mv /data/web_static/releases/{file_name}/web_static/* "
+            f"/data/web_static/releases/{file_name}/")
+        run("rm -rf /data/web_static/current")
+        run(f"ln -s /data/web_static/releases/{file_name}/"
+            f" /data/web_static/current")
+        return True
+    except Exception:
         return False
