97ac32a867632aebc6d6ac5df0c0342215d0f339
meedouazane <mohamedouazane@yahoo.com>
2024-01-10 14:14:12 -0800

    test exist

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 ''' creates and distributes an archive to your web servers '''
 from fabric.api import env, run, put
+from os.path import exists
 do_pack = __import__('1-pack_web_static').do_pack
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
@@ -11,7 +12,7 @@ env.hosts = ['54.167.181.186', '35.153.194.155']
 def deploy():
     ''' Full deployment '''
     archive = do_pack()
-    if not archive:
+    if exists(archive) is False:
         return False
     deploy = do_deploy(archive)
     return deploy
