0bd9140147ac51aec2e1908d723845aa867484ce
meedouazane <mohamedouazane@yahoo.com>
2024-01-10 15:37:07 -0800

    101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 # Puppet for setup
-file { /data:
+file { '/data':
   ensure => 'directory',
   owner  => 'ubuntu',
   group  => 'ubuntu'
 }
-file { /data/'web_static':
+file { '/data/web_static':
   ensure => 'directory',
   owner  => 'ubuntu',
   group  => 'ubuntu'
 }
-file { /data/web_static/'releases':
+file { '/data/web_static/releases':
   ensure => 'directory',
   owner  => 'ubuntu',
   group  => 'ubuntu'
 }
-file { /data/web_static/releases/test/:
+file { '/data/web_static/releases/test/':
   ensure => 'directory',
   owner  => 'ubuntu',
   group  => 'ubuntu'
 }
-file { /data/web_static/shared/:
+file { '/data/web_static/shared/':
   ensure => 'directory',
   owner  => 'ubuntu',
   group  => 'ubuntu'
