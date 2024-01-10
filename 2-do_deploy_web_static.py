f44805b7dd8808928b29fae3f54595dbbac4169b
meedouazane <mohamedouazane@yahoo.com>
2024-01-10 14:50:40 -0800

    2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,11 +15,11 @@ def do_deploy(archive_path):
         file_name_ext = archive_path.split('/')[1]
         file_name = file_name_ext.split('.')[0]
         run(f"mkdir -p /data/web_static/releases/{file_name}/")
-        run(f" tar -xzf /tmp/{file_name_ext} -C "
+        run(f"tar -xzf /tmp/{file_name_ext} -C "
             f"/data/web_static/releases/{file_name}/")
         run(f"rm -rf /tmp/{file_name_ext}")
-        run(f"mv /data/web_static/releases/{file_name}/web_static/* "
-            f"/data/web_static/releases/{file_name}/")
+        '''run(f"mv /data/web_static/releases/{file_name}/web_static/* "
+            f"/data/web_static/releases/{file_name}/")'''
         run("rm -rf /data/web_static/current")
         run(f"ln -s /data/web_static/releases/{file_name}/"
             f" /data/web_static/current")
