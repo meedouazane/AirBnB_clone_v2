d304bffa1fb300624d2dd96f5caa7ef327f40959
meedouazane <mohamedouazane@yahoo.com>
2024-01-11 09:29:14 -0800

    2-test

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,16 +11,16 @@ def do_deploy(archive_path):
     """ Deploy archive! """
     if exists(archive_path) is False:
         return False
+    file_name_ext = archive_path.split('/')[1]
+    file_name = file_name_ext.split('.')[0]
     try:
         put(archive_path, '/tmp/')
-        file_name_ext = archive_path.split('/')[1]
-        file_name = file_name_ext.split('.')[0]
         run(f"mkdir -p /data/web_static/releases/{file_name}/")
         run(f"tar -xzf /tmp/{file_name_ext} -C "
             f"/data/web_static/releases/{file_name}/")
         run(f"rm /tmp/{file_name_ext}")
-        run(f"mv /data/web_static/releases/{file_name}/web_static/* "
-            f"/data/web_static/releases/{file_name}/")
+        '''run(f"mv /data/web_static/releases/{file_name}/web_static/* "
+            f"/data/web_static/releases/{file_name}/")'''
         run(f"rm -rf /data/web_static/releases/{file_name}/web_static")
         run("rm -rf /data/web_static/current")
         run(f"ln -s /data/web_static/releases/{file_name}/"
