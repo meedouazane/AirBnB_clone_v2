f3bc24deed2d4c2696fa8259189049d51685e95b
meedouazane <mohamedouazane@yahoo.com>
2024-01-11 09:17:28 -0800

    3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python3
 ''' creates and distributes an archive to your web servers '''
-from fabric.api import env, run, put
+from fabric.api import env, run, put, local
 from os.path import exists
-do_pack = __import__('1-pack_web_static').do_pack
+from datetime import datetime
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
 
 env.hosts = ['54.167.181.186', '35.153.194.155']
+env.hosts = ['localhost']
 
+
+def do_pack():
+    ''' Compress Files '''
+
+    local("mkdir -p versions")
+    currtime = datetime.now().strftime('%Y%m%d%H%M%S')
+    path = f"versions/'web_static_{currtime}.tgz'"
+    generate = local(f"tar -czvf {path} -C web_static .")
+    if generate:
+        return path
+    else:
+        return None
 
 def deploy():
     ''' Full deployment '''
