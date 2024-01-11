ed590def30a50995df8bbce1966ccf3110dd2e8a
meedouazane <mohamedouazane@yahoo.com>
2024-01-11 12:43:00 -0800

    3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 ''' creates and distributes an archive to your web servers '''
 from fabric.api import env, run, put, local
-from os.path import exists
 from datetime import datetime
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
 
 env.hosts = ['54.167.181.186', '35.153.194.155']
-env.hosts = ['localhost']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/school'
 
 
 def do_pack():
@@ -16,16 +16,17 @@ def do_pack():
     local("mkdir -p versions")
     currtime = datetime.now().strftime('%Y%m%d%H%M%S')
     path = f"versions/'web_static_{currtime}.tgz'"
-    generate = local(f"tar -czvf {path} -C web_static .")
+    generate = local(f"tar -czvf {path}  web_static")
     if generate:
         return path
     else:
         return None
 
+
 def deploy():
     ''' Full deployment '''
     archive = do_pack()
-    if exists(archive) is False:
+    if not archive:
         return False
     deploy = do_deploy(archive)
     return deploy
