37de0d7268876435635471726235ce5ca67a139a
meedouazane <mohamedouazane@yahoo.com>
2024-01-12 08:10:48 -0800

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -24,8 +24,8 @@ def do_deploy(archive_path):
         run(f"tar -xzf /tmp/{file_name_ext} -C "
             f"/data/web_static/releases/{file_name}/")
         run(f"rm /tmp/{file_name_ext}")
-        '''run(f"mv /data/web_static/releases/{file_name}/web_static/* "
-            f"/data/web_static/releases/{file_name}/")'''
+        run(f"mv /data/web_static/releases/{file_name}/web_static/* "
+            f"/data/web_static/releases/{file_name}/")
         run(f"rm -rf /data/web_static/releases/{file_name}/web_static")
         run("rm -rf /data/web_static/current")
         run(f"ln -s /data/web_static/releases/{file_name}/"
