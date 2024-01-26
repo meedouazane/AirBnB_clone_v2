b5f1f0bfe0726cef35eb9aeed2cde78a4785b46a
Ouazane <mohamedouazane@yahoo.com>
2024-01-26 03:49:10 -0800

    3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+''' starts a Flask web application '''
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text_c(text):
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route('/python', strict_slashes=False)
+def text_python_default():
+    return "Python is cool"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def text_python(text):
+    if (text):
+        text = text.replace('_', ' ')
+        return f"Python {text}"
+    else:
+        return "Python is cool"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
