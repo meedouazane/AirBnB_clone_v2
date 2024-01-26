48fc7b57d94ae1ac6526eec3c3fcb0cdb929b421
Ouazane <mohamedouazane@yahoo.com>
2024-01-26 12:56:21 -0800

    6-number_odd_or_even.py

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+''' starts a Flask web application '''
+from flask import Flask, render_template
+
+
+app = Flask(__name__, template_folder='templates/')
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text_c(text):
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route('/python', strict_slashes=False)
+def text_python_default():
+    return "Python is cool"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def text_python(text):
+    text = text.replace('_', ' ')
+    return f"Python {text}"
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def n_type(n):
+    try:
+        n = int(n)
+        return f"{n} is a number"
+    except ValueError:
+        pass
+
+
+@app.route('/number_template/<n>', strict_slashes=False)
+def n_type_html(n):
+    try:
+        n = int(n)
+        return render_template('5-number.html', number=n)
+    except ValueError:
+        pass
+
+
+@app.route('/number_odd_or_even/<n>', strict_slashes=False)
+def even_odd(n):
+    try:
+        n = int(n)
+        return render_template('6-number_odd_or_even.html', number=n)
+    except ValueError:
+        pass
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
