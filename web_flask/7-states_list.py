dc60bcc391af75ecc758a7ad8903f8c9c7b771e0
Ouazane <mohamedouazane@yahoo.com>
2024-01-27 05:54:36 -0800

    7

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+''' starts a Flask web application '''
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__, template_folder='templates/')
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_html():
+    all_states = storage.all(State)
+    return render_template('7-states_list.html', states=all_states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
