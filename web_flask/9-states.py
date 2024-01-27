92d41aec483b681e6749c676c25995fed3655e24
Ouazane <mohamedouazane@yahoo.com>
2024-01-27 13:13:28 -0800

    9

---
+++ web_flask/9-states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+''' starts a Flask web application '''
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__, template_folder='templates/')
+
+
+@app.route('/states', strict_slashes=False)
+def states_html():
+    all_states = storage.all()
+    return render_template('9-states.html', states=all_states)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def states_id(id):
+    all_states = storage.all(State)
+    id = 'state.' + id
+    return render_template('9-states.html', states=all_states, s_id=id)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    ''' remove the current SQLAlchemy Session '''
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
