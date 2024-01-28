62c47659c4a147a92b349dc881a57fd336d6969d
Ouazane <mohamedouazane@yahoo.com>
2024-01-28 08:36:58 -0800

    state.py

--- models/state.py
+++ models/state.py
@@ -24,16 +24,17 @@ class State(BaseModel, Base):
     else:
         name = ""
 
-        @property
-        def cities(self):
-            """Returns the list of City instances with
-            state_id equals to the current State.id"""
-            from models import storage
-            from models.city import City
+        if models.storage_t != "db":
+            @property
+            def cities(self):
+                """Returns the list of City instances with
+                state_id equals to the current State.id"""
+                from models import storage
+                from models.city import City
 
-            all_cities = storage.all(City)
-            return [
-                city
-                for city in all_cities.values()
-                if city.state_id == self.id
-            ]
+                all_cities = storage.all(City)
+                return [
+                    city
+                    for city in all_cities.values()
+                    if city.state_id == self.id
+                ]
